16f3939965ff8255323e961c3b47e2bb8ba5f03a
sayuuuto <thehazardousway@live.fr>
2017-08-22 17:32:04 +0100

    fb.txt

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//file_put_contents("fb.txt",file_get_contents("php://input"));
+file_put_contents("fb.txt",file_get_contents("php://input"));
 $json = file_get_contents('php://input');
 $data=json_decode($json);
 $id = $data->entry[0]->id;
@@ -9,5 +9,5 @@ $recipient_id =$data->entry[0]->messaging[0]->recipient->id;
 $sender_id = $data->entry[0]->messaging[0]->sender->id;
 $timestamp = $data->entry[0]->messaging[0]->timestamp;
 $problem_id = $data->entry[0]->id;
-file_put_contents("fb.txt",date('m/d/Y H:i:s', $timestamp));
+//file_put_contents("fb.txt",date('m/d/Y H:i:s', $timestamp));
 var_dump($data);
