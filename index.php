de12d7040d8e14abb7a2349022e8fadcc2ebf463
sayuuuto <thehazardousway@live.fr>
2017-08-22 17:29:27 +0100

    fb.txt

--- index.php
+++ index.php
@@ -9,5 +9,5 @@ $recipient_id =$data->entry[0]->messaging[0]->recipient->id;
 $sender_id = $data->entry[0]->messaging[0]->sender->id;
 $timestamp = $data->entry[0]->messaging[0]->timestamp;
 $problem_id = $data->entry[0]->id;
-file_put_contents("fb.txt",$timestamp);
+file_put_contents("fb.txt",date('m/d/Y', $timestamp));
 var_dump($data);
