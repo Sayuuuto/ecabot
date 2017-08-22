84f11313d9e2252b2f151c469b382881cbfbb118
sayuuuto <thehazardousway@live.fr>
2017-08-22 18:38:18 +0100

    fb.txt

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $json = file_get_contents('php://input');
-file_put_contents("fb.txt",$_SERVER['REMOTE_ADDR']);
+file_put_contents("fb.txt",$_SERVER['HTTP_CLIENT_IP']);
 $data=json_decode($json);
 $id = $data->entry[0]->id;
 $msg_text = $data->entry[0]->messaging[0]->message->text;
