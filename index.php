5fa7d7ca85300bb6d9b7d36c6609105894c155bb
sayuuuto <thehazardousway@live.fr>
2017-08-22 17:01:36 +0100

    aze

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: aziane
- * Date: 22/08/2017
- * Time: 16:55
- */
-echo 'hi';
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+<script>
+    app.get('/webhook', function(req, res) {
+        if (req.query['hub.mode'] === 'subscribe' &&
+            req.query['hub.verify_token'] === <eca_bot>) {
+            console.log("Validating webhook");
+            res.status(200).send(req.query['hub.challenge']);
+        } else {
+            console.error("Failed validation. Make sure the validation tokens match.");
+            res.sendStatus(403);
+        }
+    });
+</script>
+</body>
+</html>
