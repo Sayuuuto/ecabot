05908766e059acd32aef5d55bf6c35b60b107dba
sayuuuto <thehazardousway@live.fr>
2017-08-22 17:05:07 +0100

    aze

--- index.php
+++ index.php
@@ -1,20 +1,2 @@
-<!doctype html>
-<html>
-<head>
-
-</head>
-<body>
-<script>
-    app.get('/', function(req, res) {
-        if (req.query['hub.mode'] === 'subscribe' &&
-            req.query['hub.verify_token'] === <eca_bot>) {
-            console.log("Validating webhook");
-            res.status(200).send(req.query['hub.challenge']);
-        } else {
-            console.error("Failed validation. Make sure the validation tokens match.");
-            res.sendStatus(403);
-        }
-    });
-</script>
-</body>
-</html>
+<?php
+print_r($_GET);
