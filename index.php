dbb395d92b392f4bf5e1d39c748fa284146141a1
sayuuuto <thehazardousway@live.fr>
2017-08-23 12:28:17 +0100

    fb.txt

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $options = array(
     )
 );
 $context = stream_context_create($options);
-if(!isset($i)) $i=0;
+
 try {
 
     $conn = new PDO("mysql:dbname=sql11191189;host=sql11.freesqldatabase.com", $username, $password);
@@ -39,8 +39,8 @@ try {
     $stmt->bindParam(':sender_id', $sender_id);
     $stmt->execute();
     file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token",false,$context);
-    $i++;
-    echo $i;
+
+    echo $msg_text;
 }
 catch(PDOException $e)
 {
