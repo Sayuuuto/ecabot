ade1b48ac223943b55022fb9a37b2fa59936045c
sayuuuto <thehazardousway@live.fr>
2017-08-23 09:59:52 +0100

    fb.txt

--- index.php
+++ index.php
@@ -42,4 +42,4 @@ $options = array(
     )
 );
 $context = stream_context_create($options);
-file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token",false,$context);
+//file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token",false,$context);
