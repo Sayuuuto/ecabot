21beb7cd171e4d425d9fd964caaf080ef5f1af4d
sayuuuto <thehazardousway@live.fr>
2017-09-04 09:49:30 +0100

    fb.txt

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $dbname = "api_facebook";
 
 $sender_data  = json_decode(file_get_contents("https://graph.facebook.com/$sender_id?access_token=EAADKrOX6ciMBAAzNOosNKaXIYGxLMPtYp3LvkLAx3D09Eg3deSTTcPGUemrUIKyxI0tyZCsLIq3ImPCC0amgFFl6IZAljFMWDx4hzW1eQFtVdQy8tNACH3Qs0z3O33PCNRw5pic27dcvYl6zv8kaCIEZBlneasWT13Pfn5ItQZDZD"));
 $sender_name= $sender_data->first_name . " " .$sender_data->last_name;
-
+$sender_pic = $sender_data->profile_pic;
 //$data = array(
 //    'recipient' => array('id'=> "$sender_id"),
 //    'message' => array('text' => "Bien recu")
@@ -38,8 +38,8 @@ $sender_name= $sender_data->first_name . " " .$sender_data->last_name;
 try {
     if(isset($msg_text)){
     $conn = new PDO("mysql:dbname=sql11191189;host=sql11.freesqldatabase.com", $username, $password);
-    $sql = 'INSERT INTO messages( msg_text, msg_id, recipient_id, sender_id, sended_at,type_message,sender_name) 
-							 VALUES(:msg_text,:msg_id,:recipient_id,:sender_id,:sended_at,"recieved",:sender_name)';
+    $sql = 'INSERT INTO messages( msg_text, msg_id, recipient_id, sender_id, sended_at,type_message,sender_name,sender_pic) 
+							 VALUES(:msg_text,:msg_id,:recipient_id,:sender_id,:sended_at,"recieved",:sender_name,:sender_pic)';
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':msg_text', $msg_text);
     $stmt->bindParam(':msg_id', $msg_id);
@@ -47,6 +47,7 @@ try {
     $stmt->bindParam(':sender_id', $sender_id);
     $stmt->bindParam(':sended_at',$sended_at);
     $stmt->bindParam(':sender_name',$sender_name);
+    $stmt->bindParam(':sender_pic',$sender_pic);
     $stmt->execute();
     //file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token",false,$context);
     }
