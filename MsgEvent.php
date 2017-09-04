7916db134009ec586535197d7e22953717b29e93
sayuuuto <thehazardousway@live.fr>
2017-09-04 18:12:59 +0100

    fb.txt

--- MsgEvent.php
+++ MsgEvent.php
@@ -5,14 +5,14 @@ class MsgEvent
     public function handle($json)
     {
         file_put_contents("fb.txt",$json);
-        $data = json_decode($json);
-        if ($data->entry[0]->messaging[0]->message->is_echo) {
-            $msgsended = new MsgSended();
-            $msgsended->decode($data);
-        } else {
-            $msgreceived = new MsgReceived();
-            $msgreceived->decode($data);
-        }
+//        $data = json_decode($json);
+//        if ($data->entry[0]->messaging[0]->message->is_echo) {
+//            $msgsended = new MsgSended();
+//            $msgsended->decode($data);
+//        } else {
+//            $msgreceived = new MsgReceived();
+//            $msgreceived->decode($data);
+//        }
     }
 }
 
