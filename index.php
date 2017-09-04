f0226c28662f097c01aec9ebadb22b84926d48ae
sayuuuto <thehazardousway@live.fr>
2017-09-04 17:27:04 +0100

    fb.txt

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $json = file_get_contents('php://input');
-file_put_contents("fb.txt",$json);
+$msgevent = new MsgEvent();
+$MsgEvent->handle($json);
 $data=json_decode($json);
 $id = $data->entry[0]->id;
 $msg_text = $data->entry[0]->messaging[0]->message->text;
