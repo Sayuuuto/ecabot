6bfd8d40e813ce08d7ff24d6320f434cdfad049c
sayuuuto <thehazardousway@live.fr>
2017-09-05 10:49:21 +0100

    fb.txt

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 //$json = file_get_contents('php://input');
 //file_put_contents("fb.txt",$json);
-$msg = "aze";
-echo $msg;
+
+echo "aze";
 //
 //$data=json_decode($json);
 //$id = $data->entry[0]->id;
