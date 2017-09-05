783d3f67a7b845a56e4937817ecbac8a4ce75b24
sayuuuto <thehazardousway@live.fr>
2017-09-05 10:07:45 +0100

    fb.txt

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 //$json = file_get_contents('php://input');
 //file_put_contents("fb.txt",$json);
-echo "haze";
+$msg = new MsgEvent;
+dd($msg);
 //
 //$data=json_decode($json);
 //$id = $data->entry[0]->id;
