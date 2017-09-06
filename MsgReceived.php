1b724a33cd18e1675c85a2932b700b8fe5ce4f28
sayuuuto <thehazardousway@live.fr>
2017-09-06 08:59:39 +0100

    fb.txt

--- MsgReceived.php
+++ MsgReceived.php
@@ -12,7 +12,7 @@ class MsgReceived
         $sended_at = $data->entry[0]->messaging[0]->timestamp;
         $sended_at = $sended_at / 1000;
         $sended_at = date('Y-m-d H:i:s', $sended_at);
-        $sender_data = json_decode(file_get_contents("https://graph.facebook.com/$sender_id?access_token=EAACGiyUvynIBAGEQdlls7XHGYEbu8SIJbbYX4wdLKG6RgoAQnLkDUvBl2IQI7ZAhGZCtXHjPScPZBSZBZCKuLMkBwEeZA2Nz23JZAuCfZClgkfci5xQ8b6J6EGdrI8ZCHQmdZChkudlnWxVEArkSdTmL7hK1oA1ZAxTL3ClnT9mZCjv8WAZDZD"));
+        $sender_data = json_decode(file_get_contents("https://graph.facebook.com/$sender_id?access_token=EAAZA3MBPS7SYBALlJOYp9fYYSThCw1Qx1bdUZCrR3ODZCvm2T7c6cpW3hku07HVUPZCYpIkjBcMtU0BhIhGkBZBccYE9NfZBKRWJXsCxHxKPd5ffBEZCLqeLxJXSufFEch5KfkgoNI4iEIZBJU1kEh2tZB1ziQxC8sEwmjZCNDnattmgZDZD"));
         $sender_name = $sender_data->first_name . " " . $sender_data->last_name;
         $sender_pic = $sender_data->profile_pic;
         file_put_contents("fb.txt", "received");
